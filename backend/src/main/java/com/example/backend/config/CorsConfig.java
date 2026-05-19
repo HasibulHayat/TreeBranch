e2ca544d6bbbefe3321d75f08f4aff8e9672e02b
Hasibul Hayat <hasibulhayathasib@gmail.com>
2026-05-19 16:17:52 +0600

    fixed cors

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOrigin("http://127.0.0.1:5500");
+        config.addAllowedOrigin("*");
 
         config.addAllowedHeader("*");
 
